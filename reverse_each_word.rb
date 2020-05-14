9913f68747663c911baa7b888c8820c05586db9c
Anette Rivera <anettem.rivera@gmail.com>
2020-05-14 20:35:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,10 +6,10 @@ def reverse_each_word(sentence)
   end 
 end 
 
-def reverse_each_word(sentence)
-  sentence_arr = []
-  sentence_arr << sentence.split(" ")
-  sentence_arr.collect do |words|
-  return words.reverse.join(" ").reverse
-  end 
-end 
+# def reverse_each_word(sentence)
+#   sentence_arr = []
+#   sentence_arr << sentence.split(" ")
+#   sentence_arr.collect do |words|
+#   return words.reverse.join(" ").reverse
+#   end 
+# end 
